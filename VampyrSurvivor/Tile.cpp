5435364dd075f2441e800519edbd03603d470c89
Fuegodavid57 <gregza@hotmail.fr>
2024-03-20 09:03:36 +0100

    Map sprite

--- VampyrSurvivor/Tile.cpp
+++ VampyrSurvivor/Tile.cpp
@@ -1,16 +1,12 @@
 #include "Tile.h"
 #include "Macro.h"
 
-Tile::Tile(const TileType& _type, const Vector2f& _pos) : Actor(STRING_ID("Tile"),ShapeData(_pos, Vector2f(TILE_SIZE)))
+Tile::Tile(const TileType& _type, const Vector2f& _pos) : Actor(STRING_ID("Tile"),ShapeData(_pos, Vector2f(TILE_SIZE),GetPathWithType(_type)))
 {
 	type = _type;
-
-	Color _color = type == TT_GRASS ? Color::Red : Color::Green;
-
-	shape->setFillColor(_color);
-	shape->setOutlineColor(Color::Black);
-	shape->setOutlineThickness(-1.f);
 	shape->setOrigin(Vector2f(0.f, 0.f));
+	shape->setOutlineThickness(-2.f);
+	shape->setOutlineColor(Color::White);
 }
 
 void Tile::ResetTileCost()
